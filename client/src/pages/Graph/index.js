fc27ae3e32e71c3063a39e0ed5d6bf6cc809c05c
Allison Kopp <allison.kopp03@gmail.com>
2019-05-25 18:12:58 -0400

    bar chart complete

--- client/src/pages/Graph/index.js
+++ client/src/pages/Graph/index.js
@@ -1,15 +1,21 @@
 import React, { Component } from 'react';
 import { SectionWrapper, LineGraph, BarGraph } from '../../components';
-import { labels, calculateMax, createObjArr } from '../../utils';
+import { labels, calculateMax, createObjArr, filterBySeason } from '../../utils';
 
 class Graph extends Component {
   state = {
     data: [],
-    barData: []
+    barData: [],
+    season: String()
   };
+
+  componentDidMount() {
+    // this.createBarData();
+  }
 
   componentWillReceiveProps(props) {
     this.createDataSet(props);
+    this.createBarData(props);
   }
 
   createDataSet = props => {
@@ -17,22 +23,38 @@ class Graph extends Component {
     this.setState({ data: dataSet });
   };
 
+  //fix hardcoded value
+  createBarData = props => {
+    const dataSet = createObjArr(labels, filterBySeason(props.issues, 'Spring'));
+    this.setState({ barData: dataSet });
+  };
+
+  getSeason = e => this.setState({ season: e.target.name });
+
   render() {
     const getTopIssue = _ => {
       if (this.state.data.length) return calculateMax(this.state.data).issue;
     };
 
-    const test = _ => {
-      if (this.props.issues.length) return this.props.issues.map(x => x.date.splice(x.date.splice.indexOf('-'), 2));
-    };
-
-    console.log(test(), 'please work');
-
     return (
       <SectionWrapper>
+        <h1>Regular data set</h1>
         <LineGraph data={this.state.data} xAxisLabel={'issue'} dataKey={'frequency'} />
-        <h3>The top issue is {getTopIssue()}</h3>
-        <BarGraph data={this.state.data} xAxisLabel={'issue'} dataKey={'frequency'} />
+        <p>The top issue is {getTopIssue()}</p>
+        <h1>Data set by season</h1>
+        <button name="Spring" type="button" onClick={this.getSeason}>
+          Spring
+        </button>
+        <button name="Summer" type="button" onClick={this.getSeason}>
+          Summer
+        </button>
+        <button name="Fall" type="button" onClick={this.getSeason}>
+          Fall
+        </button>
+        <button name="Winter" type="button" onClick={this.getSeason}>
+          Winter
+        </button>
+        <BarGraph data={this.state.barData} xAxisLabel={'issue'} dataKey={'frequency'} />
       </SectionWrapper>
     );
   }
